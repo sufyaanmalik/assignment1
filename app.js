68ed8666ab8db5a08811441b6c26b95e9f80259e
sufyaanmalik <sufyaanmaalik@gmail.com>
2024-12-18 09:22:09 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Chapter 1 
+
+// Question 1
+
+alert("Hello");
+
+// Question 2
+
+document.write("Hello");
+
+// Question 3
+
+document.write("<br><br>" + "Hello I am Sufyan Malik" + "<br>" + "A Web Developer");
+
+// Question 5
+
+console.log("Hello World");
